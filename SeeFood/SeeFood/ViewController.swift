79356bf5e7e7314032f9962f1591ae596767f207
Jervy Umandap <jegumandap@unionbankph.com>
2024-09-30 18:29:27 +0800

    ### Camera functionality
    *Embedded ViewController to Navigation
    *Added NSCameraUsageDescription, NSPhotoLibraryUsageDescription to info.plist
    *IBOutlet, IBActions, imagePicker delegate and methods.

--- SeeFood/SeeFood/ViewController.swift
+++ SeeFood/SeeFood/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePickerSetup()
     }
-
-
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerSetup() {
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
 }
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let userPickedImage = info[.originalImage] as? UIImage else {
+            return
+        }
+        imageView.image = userPickedImage
+        imagePicker.dismiss(animated: true)
+    }
+}
+
+extension ViewController: UINavigationControllerDelegate {
+    
+}
+
+
 
