eacc17eca7f9ec81389804c0e987634ba33357c7
Jervy Umandap <jegumandap@unionbankph.com>
2024-09-30 17:53:38 +0800

    ### Starter project

---
+++ SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Jervy Umandap on 9/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
